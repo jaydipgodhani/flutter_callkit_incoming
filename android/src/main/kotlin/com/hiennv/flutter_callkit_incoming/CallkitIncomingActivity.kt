9a17de7065626235ed4440394031f2a412713e2b
Jaydip Godhani <godhani.jaydip145@gmail.com>
2023-02-17 12:50:52 +0530

    Change intent

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
@@ -301,7 +301,7 @@ class CallkitIncomingActivity : Activity() {
     private fun onDeclineClick() {
         val soundPlayerServiceIntent =
                 Intent(applicationContext, CallkitSoundPlayerService::class.java)
-        val pendingIntent: PendingIntent = PendingIntent.getBroadcast(applicationContext, 123, intent, 0)
+        val pendingIntent: PendingIntent = PendingIntent.getBroadcast(applicationContext, 123, soundPlayerServiceIntent, 0)
 
 
         val data = intent.extras?.getBundle(EXTRA_CALLKIT_INCOMING_DATA)
